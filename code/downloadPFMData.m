320eb8f3c87db96a0a4ee51c720408c9b818af0a
mjmarin <mjmarinj@yahoo.es>
2016-05-09 17:17:58 +0200

    mjmarin: updated urls

--- code/downloadPFMData.m
+++ code/downloadPFMData.m
@@ -16,10 +16,43 @@ if ~exist(filename, 'file')
    fprintf(' done!\n');
 end
 
-%% Identify classifier
+%% PFM-visual classifier
 filename = 'full_model_svmlin_K=0600_PCAH256_nFrames=0000_overlap=00.mat';
 if ~exist(filename, 'file')
    zipfile = 'svm_pfm.zip';
+   theURL = ['http://rabinf24.uco.es/gaitdata/' zipfile];      
+   fprintf('+ Downloading file %s ...', zipfile);
+   urlwrite(theURL, zipfile);
+   unzip(zipfile, outdir);
+   fprintf(' done!\n');
+end
+
+%% PFM-depth classifier
+filename = 'full_model_svmlin_K=0600_PCAH256_nFrames=0000_overlap=00_depth.mat';
+if ~exist(filename, 'file')
+   zipfile = 'svm_depth.zip';
+   theURL = ['http://rabinf24.uco.es/gaitdata/' zipfile];      
+   fprintf('+ Downloading file %s ...', zipfile);
+   urlwrite(theURL, zipfile);
+   unzip(zipfile, outdir);
+   fprintf(' done!\n');
+end
+
+%% Gender classifier
+filename = 'full_model_svmlin_K=0600_PCAH256_nFrames=0000_overlap=00_gender.mat';
+if ~exist(filename, 'file')
+   zipfile = 'svm_gender.zip';
+   theURL = ['http://rabinf24.uco.es/gaitdata/' zipfile];      
+   fprintf('+ Downloading file %s ...', zipfile);
+   urlwrite(theURL, zipfile);
+   unzip(zipfile, outdir);
+   fprintf(' done!\n');
+end
+
+%% Shoes classifier
+filename = 'full_model_rb_K=0600_PCAH256_nFrames=0000_overlap=00.mat';
+if ~exist(filename, 'file')
+   zipfile = 'rb_shoes.zip';
    theURL = ['http://rabinf24.uco.es/gaitdata/' zipfile];      
    fprintf('+ Downloading file %s ...', zipfile);
    urlwrite(theURL, zipfile);
